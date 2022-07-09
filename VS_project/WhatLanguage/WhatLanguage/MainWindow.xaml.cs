beaa5944fc2415d079605ab40b71ed1f99a85057
Kacper <thefisher21@gmail.com>
2022-07-09 11:21:55 +0200

    Many windows with movement between added

--- VS_project/WhatLanguage/WhatLanguage/MainWindow.xaml.cs
+++ VS_project/WhatLanguage/WhatLanguage/MainWindow.xaml.cs
@@ -23,21 +23,38 @@ namespace WhatLanguage
     {
         OOProjectEntities context = new OOProjectEntities();
         CollectionViewSource langViewSource;
-
+        /// <summary>
+        /// Initializing Data
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
             langViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("langViewSource1")));
             DataContext = this;
         }
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             context.Lang.Load();
             langViewSource.Source = context.Lang.Local;
+        }
 
-            // Load data by setting the CollectionViewSource.Source property:
-            // langViewSource1.Source = [generic data source]
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void OpenWindow1(object sender, RoutedEventArgs e)
+        {
+
+            Window1 objWin1 = new Window1();
+            this.Visibility = Visibility.Hidden;
+            objWin1.Show();
+        }
+
+        private void OpenWindow2(object sender, RoutedEventArgs e)
+        {
+            Window2 objWin2 = new Window2();
+            this.Visibility = Visibility.Hidden;
+            objWin2.Show();
         }
     }
 }
---
+++ VS_project/WhatLanguage/WhatLanguage/Window2.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WhatLanguage
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void BackToMainn(object sender, RoutedEventArgs e)
+        {
+            MainWindow objMW = new MainWindow();
+            this.Visibility = Visibility.Hidden;
+            objMW.Show();
+        }
+    }
+}
