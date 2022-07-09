323e9a9b09bc5a166c756b250d707924e5cbfef0
Kacper <thefisher21@gmail.com>
2022-07-09 11:47:28 +0200

    Frameworks_notSorted added

--- VS_project/WhatLanguage/WhatLanguage/Window1.xaml.cs
+++ VS_project/WhatLanguage/WhatLanguage/Window1.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.Entity;
 
 namespace WhatLanguage
 {
@@ -19,16 +20,28 @@ namespace WhatLanguage
     /// </summary>
     public partial class Window1 : Window
     {
+        OOProjectEntities context = new OOProjectEntities();
+        CollectionViewSource frameworkViewSource;
         public Window1()
         {
             InitializeComponent();
+            frameworkViewSource = (frameworkViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("frameworkViewSource"))));
+            DataContext = this;
         }
-
         private void BackToMain(object sender, RoutedEventArgs e)
         {
             MainWindow objMW = new MainWindow();
             this.Visibility = Visibility.Hidden;
             objMW.Show();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            context.Framework.Load();
+            frameworkViewSource.Source = context.Framework.Local;
+            //System.Windows.Data.CollectionViewSource frameworkViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("frameworkViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // frameworkViewSource.Source = [generic data source]
         }
     }
 }
--- VS_project/WhatLanguage/WhatLanguage/Window2.xaml.cs
+++ VS_project/WhatLanguage/WhatLanguage/Window2.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.Entity;
 
 namespace WhatLanguage
 {
@@ -19,9 +20,13 @@ namespace WhatLanguage
     /// </summary>
     public partial class Window2 : Window
     {
+        OOProjectEntities context = new OOProjectEntities();
+        CollectionViewSource frameworkViewSource;
         public Window2()
         {
             InitializeComponent();
+            frameworkViewSource = (frameworkViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("frameworkViewSource"))));
+            DataContext = this;
         }
 
         private void BackToMainn(object sender, RoutedEventArgs e)
@@ -29,6 +34,15 @@ namespace WhatLanguage
             MainWindow objMW = new MainWindow();
             this.Visibility = Visibility.Hidden;
             objMW.Show();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            context.Framework.Load();
+            frameworkViewSource.Source = context.Framework.Local;
+            //System.Windows.Data.CollectionViewSource frameworkViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("frameworkViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // frameworkViewSource.Source = [generic data source]
         }
     }
 }
